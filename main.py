e8504e2463eb06a03febf4394a67e80f83b7526d
zhourd <10412863@qq.com>
2022-08-12 16:43:17 +0800

    1

--- main.py
+++ main.py
@@ -11,7 +11,31 @@ from module_main_page.module_main_page_logic import module_main_page_logic
 class MainWindow(QMainWindow, module_main_page_logic):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.__connect()
 
+    def __connect(self):
+        self.pushButton_main.clicked.connect(self.on_stack_control)
+        self.pushButton_module1.clicked.connect(self.on_stack_control)
+        self.pushButton_module2.clicked.connect(self.on_stack_control)
+        self.pushButton_module3.clicked.connect(self.on_stack_control)
+
+    # def on_pushbutton_main(self):
+    #     print("on_pushbutton_main")
+    #     self.stackedWidget.setCurrentIndex(0)
+    #
+    # def on_pushbutton_module1(self):
+    #     print("module1")
+    #     self.stackedWidget.setCurrentIndex(1)
+
+    def on_stack_control(self):  # 页面控制函数
+        sender = self.sender().objectName()  # 获取当前信号 sender
+        index = {
+            "pushButton_main": 0,  # page_0
+            "pushButton_module1": 1,  # page_1
+            "pushButton_module2": 2,  # page_2
+            "pushButton_module3": 3,  # page_2
+        }
+        self.stackedWidget.setCurrentIndex(index[sender])  # 根据信号 index 设置所显示的页面
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
